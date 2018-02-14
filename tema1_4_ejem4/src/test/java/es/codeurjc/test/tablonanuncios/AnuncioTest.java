077045471780e48666fae46a86fa2cc31b3ac67b
Micael Gallego <micael.gallego@gmail.com>
2018-02-14 06:29:43 +0100

    Add tests in tema1_4-ejem4

---
+++ tema1_4_ejem4/src/test/java/es/codeurjc/test/tablonanuncios/AnuncioTest.java
@@ -0,0 +1,54 @@
+package es.codeurjc.test.tablonanuncios;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class AnuncioTest {
+
+	WebDriver driver;
+	
+	@BeforeClass
+	public static void setupClass() {
+		ChromeDriverManager.getInstance().setup();
+	}
+	
+	@Before
+	public void setup() {
+		driver = new ChromeDriver();
+	}
+	
+	@After
+	public void teardown() {
+		if(driver != null) {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void createTest() throws InterruptedException {
+		driver.get("http://localhost:5555/");
+		
+		driver.findElement(By.linkText("Nuevo anuncio")).click();
+		
+		driver.findElement(By.name("nombre")).sendKeys("Anuncio nuevo con Selenium");
+		driver.findElement(By.name("asunto")).sendKeys("Vendo moto");
+		driver.findElement(By.name("comentario")).sendKeys("Un comentario muy largo...");
+		
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		
+		driver.findElement(By.linkText("Volver al tablón")).click();
+		
+		assertNotNull(driver.findElement(By.partialLinkText("Selenium")));
+	}
+
+}
