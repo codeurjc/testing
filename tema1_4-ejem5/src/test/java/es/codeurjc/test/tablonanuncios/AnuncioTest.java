c9445e2e04ad8eb5c3eca4819f735b501d36f54b
Patxi Gortázar <patxi.gortazar@gmail.com>
2018-02-22 15:57:37 +0100

    Add tema_1_4-ejem5

---
+++ tema1_4-ejem5/src/test/java/es/codeurjc/test/tablonanuncios/AnuncioTest.java
@@ -0,0 +1,114 @@
+package es.codeurjc.test.tablonanuncios;
+
+import static org.junit.Assert.*;
+import static org.openqa.selenium.remote.DesiredCapabilities.chrome;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class AnuncioTest {
+
+	private static String sutURL;
+	private static String eusURL;
+
+	WebDriver driver;
+	
+	@BeforeClass
+	public static void setupClass() {
+
+		String sutHost = System.getenv("ET_SUT_HOST");
+		if (sutHost == null) {
+			sutURL = "http://localhost:8080/";
+		} else {
+			sutURL = "http://" + sutHost + ":8080/";
+		}
+		System.out.println("App url: " + sutURL);
+
+		eusURL = System.getenv("ET_EUS_API");
+		if (eusURL == null) {
+			ChromeDriverManager.getInstance().setup();
+		}
+	}
+
+	@Before
+	public void setupTest() throws MalformedURLException {
+		String eusURL = System.getenv("ET_EUS_API");
+		if (eusURL == null) {
+			// Local Google Chrome
+			driver = new ChromeDriver();
+		} else {
+			// Selenium Grid in ElasTest
+			driver = new RemoteWebDriver(new URL(eusURL), chrome());
+		}
+	}
+	
+	@After
+	public void teardown() {
+		if(driver != null) {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void createTest() throws InterruptedException {
+		
+		System.out.println("Open page");
+		
+		driver.get(sutURL);
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Search link");
+		driver.findElement(By.linkText("Nuevo anuncio")).click();
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Fill in form");
+		driver.findElement(By.name("nombre")).sendKeys("Anuncio nuevo con Selenium");
+		driver.findElement(By.name("asunto")).sendKeys("Vendo moto");
+		driver.findElement(By.name("comentario")).sendKeys("Un comentario muy largo...");
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Submit");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		
+		System.out.println("Back to index");
+		driver.findElement(By.linkText("Volver al tablón")).click();
+		
+		assertNotNull(driver.findElement(By.partialLinkText("Selenium")));
+	}
+	
+	@Test
+	public void deleteTest() throws InterruptedException {
+		
+		System.out.println("Open page");
+		driver.get(sutURL);
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Search link");
+		driver.findElement(By.linkText("Pepe")).click();
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Delete");
+		driver.findElement(By.linkText("Borrar")).click();
+		
+		Thread.sleep(2000);
+		
+		assertNull(driver.findElement(By.partialLinkText("Juan")));
+	}
+
+}
