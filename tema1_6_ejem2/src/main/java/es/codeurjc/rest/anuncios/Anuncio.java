332e4bafe529996ad7d7c266909dee80c7c3f6f6
Micael Gallego <micael.gallego@gmail.com>
2018-03-22 17:32:43 +0100

    Add parameter-less constructor to Anuncio

--- tema1_6_ejem2/src/main/java/es/codeurjc/rest/anuncios/Anuncio.java
+++ tema1_6_ejem2/src/main/java/es/codeurjc/rest/anuncios/Anuncio.java
@@ -5,6 +5,10 @@ public class Anuncio {
 	private String nombre;
 	private String asunto;
 	private String comentario;
+	
+	public Anuncio() {
+		
+	}
 
 	public Anuncio(String nombre, String asunto, String comentario) {
 		super();
@@ -12,6 +16,18 @@ public class Anuncio {
 		this.asunto = asunto;
 		this.comentario = comentario;
 	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
 
 	public String getAsunto() {
 		return asunto;
