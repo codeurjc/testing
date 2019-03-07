9a1741f6242e3fce872de67d1953a06cde59e48d
micaelgallego <micael.gallego@gmail.com>
2019-03-07 19:16:37 +0100

    Add sleeps to tema1_3-ejem4

--- tema1_3_ejem4/src/test/java/es/codeurjc/test/web/WebAppTest.java
+++ tema1_3_ejem4/src/test/java/es/codeurjc/test/web/WebAppTest.java
@@ -48,18 +48,24 @@ public class WebAppTest {
 		String newTitle = "MessageTitle";
 		String newBody = "MessageBody";
 
+		Thread.sleep(1000);		
+		
 		driver.findElement(By.id("title-input")).sendKeys(newTitle);
 		driver.findElement(By.id("body-input")).sendKeys(newBody);
 
+		Thread.sleep(1000);
+		
 		driver.findElement(By.id("submit")).click();
 
+		Thread.sleep(1000);
+		
 		String title = driver.findElement(By.id("title")).getText();
 		String body = driver.findElement(By.id("body")).getText();
 
-                Thread.sleep(10000);
-                
 		assertThat(title).isEqualTo(newTitle);
 		assertThat(body).isEqualTo(newBody);
+		
+		Thread.sleep(10000);
 	}
 
 }
