79a9287c9d5b29461fcc08aa758cc59643449557
micaelgallego <micael.gallego@gmail.com>
2020-11-11 00:27:43 +0100

    Unify ejer3

--- tema1_2_ejer3/src/test/java/es/codeurjc/test/complex/ComplexAbsTest.java
+++ tema1_2_ejer3/src/test/java/es/codeurjc/test/complex/ComplexAbsTest.java
@@ -2,27 +2,33 @@ package es.codeurjc.test.complex;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import es.codeurjc.test.complex.Complex;
 
 public class ComplexAbsTest {
 
-	@ParameterizedTest
-	@CsvSource({
-		"0, 0, 0",
-		"1, 1, 1.41421",
-		"2, 2, 2.82843",
-		"5, 5, 7.07107",
-		"10, 10, 14.1421",
-		"10, 1, 10.0498",
-		"20, 2, 20.099"
-		})
-	public void absoluteTest(double real, double imag, double abs) {
-
-		Complex complex = new Complex(real, imag);
-		assertEquals(complex.abs(), abs, 0.001);
+	@ParameterizedTest(name = "{index}: ({0}).abs() == {1}")
+	@MethodSource("values")
+	public void absoluteTest(Complex complex, double result) {
+		assertEquals(complex.abs(), result, 0.001);
 	}
 	
+	public static Collection<Object[]> values() {
+	    Object[][] data = {
+		    	{ new Complex(0,0),0 },
+		        { new Complex(1,1),1.41421},
+				{ new Complex(2,2),2.82843},
+				{ new Complex(5,5),7.07107},
+				{ new Complex(10,10),14.1421},
+				{ new Complex(10,1),10.0498},
+				{ new Complex(20,2),20.099}
+		    };
+
+	    return Arrays.asList(data);
+	}
 }
