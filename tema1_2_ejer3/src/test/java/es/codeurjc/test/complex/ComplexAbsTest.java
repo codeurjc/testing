faa841ffef3d2b147cde5ea55ddefbc83ba778e8
micaelgallego <micael.gallego@gmail.com>
2020-11-11 19:35:25 +0100

    Format test

--- tema1_2_ejer3/src/test/java/es/codeurjc/test/complex/ComplexAbsTest.java
+++ tema1_2_ejer3/src/test/java/es/codeurjc/test/complex/ComplexAbsTest.java
@@ -19,15 +19,16 @@ public class ComplexAbsTest {
 	}
 	
 	public static Collection<Object[]> values() {
-	    Object[][] data = {
-		    	{ new Complex(0,0),0 },
-		        { new Complex(1,1),1.41421},
-				{ new Complex(2,2),2.82843},
-				{ new Complex(5,5),7.07107},
-				{ new Complex(10,10),14.1421},
-				{ new Complex(10,1),10.0498},
-				{ new Complex(20,2),20.099}
-		    };
+	    
+		Object[][] data = {
+			{ new Complex(0,0)  ,0       },
+			{ new Complex(1,1)  ,1.41421 },
+			{ new Complex(2,2)  ,2.82843 },
+			{ new Complex(5,5)  ,7.07107 },
+			{ new Complex(10,10),14.1421 },
+			{ new Complex(10,1) ,10.0498 },
+			{ new Complex(20,2) ,20.099  }
+		};
 
 	    return Arrays.asList(data);
 	}
